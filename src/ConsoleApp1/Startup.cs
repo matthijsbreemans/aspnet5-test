ef2dd3aa362c4ad010acbdede372a2487d74f318
Matthijs Breemans <matthijs@breemans.eu>
2015-06-01 16:39:18 +0200

    init

---
+++ src/ConsoleApp1/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Startup
+    {
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            return services.BuildServiceProvider();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+            app.UseWelcomePage();
+        }
+    }
+}
